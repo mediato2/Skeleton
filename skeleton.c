c2966a230ef3f81aaea8050b04fb494515541931
mediato2 <drunksashka@gmail.com>
2025-09-29 11:39:47 +0300

    2)

--- skeleton.c
+++ skeleton.c
@@ -3,5 +3,8 @@ int main () {
     printf("Hello, World111!\n");
     printf("Hello, World222!\n");
     printf("Hello, World222!\n");
+    printf("Hello, World222!\n");
+    printf("Hello, World222!\n");
+    printf("Hello, World222!\n");
     return 0;
 }

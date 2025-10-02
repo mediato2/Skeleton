6d2e37d4d12310093508d33a003dc50b1f7392bd
mediato2 <drunksashka@gmail.com>
2025-10-02 22:11:43 +0300

    Update skeleton.c

--- skeleton.c
+++ skeleton.c
@@ -8,7 +8,7 @@ int main () {
     printf("Hello, World111!\n");
     printf("Hello, World222!\n");
     printf("Hello, World222!\n");
-    printf("Hello, World222!\n");
+    printf("Hello, World22frferer2!\n");
     printf("Hello, World222!\n");
     printf("Hello, World222!\n");
     return 0;
